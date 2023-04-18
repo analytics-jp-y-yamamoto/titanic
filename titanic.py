795d3c88e4a031143c6c54bc3457d37ae05dc8a9
analytics-jp-y-yamamoto <129849323+analytics-jp-y-yamamoto@users.noreply.github.com>
2023-04-18 17:49:26 +0900

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -10,15 +10,15 @@ st.set_page_config(
 )
 
 # csv読み込み
-df0 = pd.read_csv('test.csv', index_col = 0)
-df1 = pd.read_csv('gender_submission.csv', index_col = 0)
+df0 = pd.read_csv('train.csv', index_col = 0)
+#df1 = pd.read_csv('gender_submission.csv', index_col = 0)
 
-df_merged = pd.merge(df0, df1, on='PassengerId')
+#df_merged = pd.merge(df0, df1, on='PassengerId')
 
 # セッション情報の初期化
 if "page_id" not in st.session_state:
     st.session_state.page_id = -1
-    st.session_state.df0 = df_merged
+    st.session_state.df0 = df0
 
 # 各種メニューの非表示設定
 hide_style = """
@@ -36,41 +36,55 @@ def main_page():
         unsafe_allow_html = True,
     )
 
-    chart_selector = st.sidebar.selectbox("グラフ選択",["bar","hist","pie"])
-    column_list_selector = st.sidebar.selectbox("内容選択", st.session_state.df0.columns.values)
-    survive_selector = st.sidebar.multiselect('生存者',('0(死亡)', '1(生存)'))
+    column_list = st.session_state.df0.columns.values
+    column_list = column_list[column_list != 'Survived']
+    column_list = column_list[column_list != 'Name']
 
+    chart_selector = st.sidebar.selectbox("グラフ選択",["bar","hist","pie"])
+    column_list_selector = st.sidebar.selectbox("内容選択", column_list)
+    survive_selector = st.sidebar.multiselect('生存者',(0, 1), default = [0, 1])
+    sex_selector = st.sidebar.multiselect('Sex', list(set(st.session_state.df0["Sex"].dropna(how='all'))), default = list(set(st.session_state.df0["Sex"].dropna(how='all'))))
+    #age_selector = st.sidebar.multiselect('Age', list(set(st.session_state.df0["Age"].dropna(how='all'))), default = list(set(st.session_state.df0["Age"].dropna(how='all'))))
+    sibsp_selector = st.sidebar.multiselect('SibSp', list(set(st.session_state.df0["SibSp"].dropna(how='all'))), default = list(set(st.session_state.df0["SibSp"].dropna(how='all'))))
+    parch_selector = st.sidebar.multiselect('Parch', list(set(st.session_state.df0["Parch"].dropna(how='all'))), default = list(set(st.session_state.df0["Parch"].dropna(how='all'))))
+    #ticket_selector = st.sidebar.multiselect('Ticket', list(set(st.session_state.df0["Ticket"].dropna(how='all'))), default = list(set(st.session_state.df0["Ticket"].dropna(how='all'))))
+    #fare_selector = st.sidebar.multiselect('Fare', list(set(st.session_state.df0["Fare"].dropna(how='all'))), default = list(set(st.session_state.df0["Fare"].dropna(how='all'))))
+    #cabin_selector = st.sidebar.multiselect('Cabin', list(set(st.session_state.df0["Cabin"].dropna(how='all'))), default = list(set(st.session_state.df0["Cabin"].dropna(how='all'))))
+    embarked_selector = st.sidebar.multiselect('Embarked', list(set(st.session_state.df0["Embarked"].dropna(how='all'))), default = list(set(st.session_state.df0["Embarked"].dropna(how='all'))))
 
     column_arr = st.session_state.df0[column_list_selector]
-    column_arr0 = st.session_state.df0[st.session_state.df0["Survived"] == 0][column_list_selector]
-    column_arr1 = st.session_state.df0[st.session_state.df0["Survived"] == 1][column_list_selector]
     kind = list(set(column_arr.dropna(how='all')))
 
+    select_arr = st.session_state.df0[
+                                        (
+                                        st.session_state.df0["Survived"].isin(survive_selector)
+                                        & st.session_state.df0["Sex"].isin(sex_selector)
+                                        #& st.session_state.df0["Age"].isin(age_selector)
+                                        & st.session_state.df0["SibSp"].isin(sibsp_selector)
+                                        & st.session_state.df0["Parch"].isin(parch_selector)
+                                        & st.session_state.df0["Embarked"].isin(embarked_selector)
+                                        )
+                                    ][column_list_selector]
     count_arr = []
-    count_arr0 = []
-    count_arr1 = []
+    count_select =[]
+
     for i in range(len(kind)):
         count_arr.append(list(column_arr).count(kind[i]))
-        count_arr0.append(list(column_arr0).count(kind[i]))
-        count_arr1.append(list(column_arr1).count(kind[i]))
+        count_select.append(list(select_arr).count(kind[i]))
 
     if chart_selector == "bar":
-        #fig1, ax1 = plt.subplots()
-        fig2, ax2 = plt.subplots()
-        #fig3, ax3 = plt.subplots()
 
-        ax2.bar(kind, count_arr, align="edge", width=0.2)
-        #ax1.set_title(column_list_selector)
+        fig, ax = plt.subplots()
+        index = np.arange(len(kind))
+        bar_width = 0.3
 
-        ax2.bar(kind, count_arr0, align="center", width=0.2)
-        #ax2.set_title(column_list_selector)
+        ax.bar(index, count_arr, width = bar_width, label='ALL')
+        ax.bar(index + bar_width, count_select, width = bar_width, label='select')
+        ax.set_title(column_list_selector)
+        ax.set_xticks(index + 0.5*bar_width, kind)
+        ax.legend()
 
-        ax2.bar(kind, count_arr1, align="edge", width=-0.2)
-        ax2.set_title(column_list_selector)
-
-        #st.pyplot(fig1)
-        st.pyplot(fig2)
-        #st.pyplot(fig3)
+        st.pyplot(fig)
 
     if chart_selector == "hist":
         bin = st.sidebar.slider('binの数を決めてください', 2, len(kind), len(kind))
@@ -81,10 +95,9 @@ def main_page():
         fig2, ax2 = plt.subplots()
 
         ax2.hist(list(column_arr), bins = bin, ec = 'navy')
-        #ax2.set_title("標準正規分布"+str(st.session_state.df0.shape[1])+"個データ")
         ax2.set_xlabel(column_list_selector)
         ax2.set_ylabel("度数")
-        ax2.set_yticks(np.arange(0,y_max,int(y_max/10)))
+        ax2.set_yticks(np.arange(0, y_max, int(y_max/10)))
 
         st.pyplot(fig2)
 
@@ -96,13 +109,6 @@ def main_page():
 
         st.pyplot(fig3)
 
-    #st.text(st.session_state.df0[st.session_state.df0["Survived"] == 0])
-
 # ページ判定
 if st.session_state.page_id == -1:
     main_page()
-
-
-#x軸とy軸のラベル表示
-#上の度数分布表でy軸が整数になるようにする
-#上の度数分布表の元データを書き換えて、正規分布のような形のヒストグラムになるようにする
